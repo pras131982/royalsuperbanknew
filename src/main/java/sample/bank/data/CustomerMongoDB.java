5833475b6347eaf32062aa70c15ef916024fe844
Prashant <pras131982@gmail.com>
2016-11-28 02:17:59 +0530

    Update

--- src/main/java/sample/bank/data/CustomerMongoDB.java
+++ src/main/java/sample/bank/data/CustomerMongoDB.java
@@ -21,11 +21,12 @@ public class CustomerMongoDB {
 			// Since 2.10.0, uses MongoClient
 			//MongoClient mongo = new MongoClient("localhost", 27017);
 			//MongoClient mongo = new MongoClient("mongodb:\\$OPENSHIFT_MONGODB_DB_HOST:\\$OPENSHIFT_MONGODB_DB_PORT");
-			//MongoClient mongo = new MongoClient("mongodb://admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com:37811/");
+			//MongoClient mongo = new MongoClient("mongodb:\\admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com:37811/");
 			//MongoClient mongo = new MongoClient("admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com", 37811);
 			//MongoClient mongo = new MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_URL"));
 			//MongoClient mongo = new MongoClient("583aedf889f5cff7ad000083-prasdev.rhcloud.com", 37811);
-			MongoClient mongo = new MongoClient("mongodb://583aedf889f5cff7ad000083-prasdev.rhcloud.com:37811/");
+			//MongoClient mongo = new MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_HOST") , Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT")));
+			MongoClient mongo = new MongoClient("mongodb:\\admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com:37811/");
 			/**** Get database ****/
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("royalsuperbank");

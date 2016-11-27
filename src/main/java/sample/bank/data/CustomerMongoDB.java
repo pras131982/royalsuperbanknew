ddf61be40748062dcd7d0cd0f3acfa2c34c0e37d
Prashant <pras131982@gmail.com>
2016-11-28 01:59:15 +0530

    Update

--- src/main/java/sample/bank/data/CustomerMongoDB.java
+++ src/main/java/sample/bank/data/CustomerMongoDB.java
@@ -22,7 +22,8 @@ public class CustomerMongoDB {
 			//MongoClient mongo = new MongoClient("localhost", 27017);
 			//MongoClient mongo = new MongoClient("mongodb:\\$OPENSHIFT_MONGODB_DB_HOST:\\$OPENSHIFT_MONGODB_DB_PORT");
 			//MongoClient mongo = new MongoClient("mongodb://admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com:37811/");
-			MongoClient mongo = new MongoClient("admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com", 37811);
+			//MongoClient mongo = new MongoClient("admin:MSbB19lPSQsU@583aedf889f5cff7ad000083-prasdev.rhcloud.com", 37811);
+			MongoClient mongo = new MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_URL"));
 			/**** Get database ****/
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("royalsuperbank");

aac0a2ed2e137098ed4c9e0cb9fd1897fada7c5b
Prashant <pras131982@gmail.com>
2017-02-06 01:51:18 +0530

    Updated for mongodb

--- src/main/java/sample/bank/data/CustomerMongoDB.java
+++ src/main/java/sample/bank/data/CustomerMongoDB.java
@@ -6,6 +6,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 
@@ -91,6 +92,59 @@ public class CustomerMongoDB {
 		}
 
 		return "SUCCESS";
+	}
+	
+	public CustomerBean getRecord(String userId) {
+		CustomerBean customer = null;
+		try {
+
+			/**** Connect to MongoDB ****/
+			// Since 2.10.0, uses MongoClient
+			//MongoClient mongo = new MongoClient("583aedf889f5cff7ad000083-prasdev.rhcloud.com", 37811);
+			MongoClient mongo = new MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_HOST") , Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT")));
+
+			/**** Get database ****/
+			// if database doesn't exists, MongoDB will create it for you
+			DB db = mongo.getDB("royalsuperbank");
+			
+			boolean auth = db.authenticate("admin", "MSbB19lPSQsU".toCharArray());
+
+			/**** Get collection / table from 'royalsuperbank' ****/
+			// if collection doesn't exists, MongoDB will create it for you
+			DBCollection table = db.getCollection("user");
+
+
+			/**** Find and display ****/
+			
+		    DBCursor cursor = table.find();
+		    while(cursor.hasNext()) {
+		        System.out.println(cursor.next());
+		    }
+		    
+		    
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put("userId", userId);
+
+			DBCursor cursor1 = table.find(searchQuery);
+			while (cursor1.hasNext()) {
+				DBObject bd = cursor1.next();
+				System.out.println(bd);
+				customer = new CustomerBean();
+				customer.setUserId((String) bd.get("userId"));
+				customer.setPassword((String) bd.get("password"));
+				customer.setAddress((String) bd.get("address"));
+				customer.setAccountBalance((Float) bd.get("openingBalance"));
+			}
+
+		
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+
+		return customer;
 	}
 
 }
--- src/main/java/sample/bank/data/Customers.java
+++ src/main/java/sample/bank/data/Customers.java
@@ -5,37 +5,48 @@ import java.util.HashMap;
 import sample.bank.bean.CustomerBean;
 
 public class Customers {
-	
+
 	private HashMap customers = new HashMap();
 
-	public void addCustomers(String userId, CustomerBean customer){
-		try{
-		customers.put(userId, customer);
-		new CustomerMongoDB().addRecord(userId, customer);
-		}
-		catch(Exception e){
+	public void addCustomers(String userId, CustomerBean customer) {
+		try {
+			customers.put(userId, customer);
+			new CustomerMongoDB().addRecord(userId, customer);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public boolean newUserId(String userId){
-		if (customers.containsKey(userId)){
+
+	public boolean newUserId(String userId) {
+		if (customers.containsKey(userId)) {
 			System.out.println("contains yaar");
 			return false;
-			
-		}
-		else {
-			System.out.println("doesnt contain");
-			return true;
+
+		} else {
+
+			CustomerBean cb = new CustomerMongoDB().getRecord(userId);
+			if (cb != null) {
+				return false;
+			} else {
+				System.out.println("doesnt contain");
+				return true;
 			}
 		}
-	
-	public CustomerBean getCustomers(String userId, String password){
+	}
+
+	public CustomerBean getCustomers(String userId, String password) {
 		CustomerBean customer;
-		if ((customer = (CustomerBean)customers.get(userId))!= null ) {
-			
-			if ( customer.getPassword().equals(password))
+		if ((customer = (CustomerBean) customers.get(userId)) != null) {
+
+			if (customer.getPassword().equals(password)) {
 				return customer;
+			}
+		} else {
+			CustomerBean cb = new CustomerMongoDB().getRecord(userId);
+			if (cb != null && cb.getPassword().equals(password)) {
+				return cb;
+			}
+
 		}
 		return null;
 	}
